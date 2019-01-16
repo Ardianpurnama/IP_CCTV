c6672a76bce7c7b550066bece0ce5b27a44a6c1d
Ardianpurnama <31305690+Ardianpurnama@users.noreply.github.com>
2019-01-16 17:04:54 +0700

    Add files via upload

---
+++ RFU_IP/us.py
@@ -0,0 +1,40 @@
+import requests,os,re
+
+b="\033[0;34m";g="\033[1;32m";w="\033[1;37m";r="\033[1;31m";y="\033[1;33m";cyan = "\033[0;36m";lgray = "\033[0;37m";dgray = "\033[1;30m";ir = "\033[0;101m";reset = "\033[0m"
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+def unitedstates():
+    global page
+    res = requests.get('https://www.insecam.org/en/bycountry/US/', headers=headers)
+    findpage = re.findall('"?page=",\s\d+', res.text)[1]
+    rfindpage = findpage.replace('page=", ', '')
+    os.system('clear')
+    print("{}        ____ ".format(r))
+    print("   _[]_/____\__n_ ")
+    print("  |_____.--.__()_|")
+    print("  |R   //# \\\    |")
+    print("{}  |F   \\\__//    | ".format(w))
+    print("  |U    '--'     | ")
+    print("{}  '--------------'----------{}------------------.  ".format(r,w))
+    print("{}  | {}Author  : {} Ardian Purnama {}     | {}INDO{}N{}{}ESIA         | ".format(r,w,r,w,r,ir,reset,w))
+    print("{}  | {}LINE : {} sirichan_gue {}| {} RFU SEKAWAN {}|".format(r,w,w,w,lgray,w))
+    print("{}  '------------------------------------{}-------'  ".format(r,w))
+    print("{}       [ {}List page : {} {}]".format(r,w,rfindpage,r))
+    run()
+    
+def run():
+    try:
+        page = input("\033[1;31m       [ \033[1;37mPage \033[1;31m]\033[1;37m> ")
+        url = ("https://www.insecam.org/en/bycountry/US/?page="+str(page))
+        print ("")
+        res = requests.get(url, headers=headers)
+        findip = re.findall('http://\d+.\d+.\d+.\d+:\d+', res.text)
+        count = 1
+        for _ in findip:
+             hasil = findip[count]
+             print ("{}[ {}{} {}]".format(r,w,hasil,r))
+             count += 1
+    except:
+        print ("")
+        print (r+"Thanks to My Team"+w)
